ccf174bca57d6cf5cf20fefec330b6f50c4fb113
Horie Ryusei <89172397+horieryusei@users.noreply.github.com>
2021-09-29 23:18:08 +0900

    Create const.php

---
+++ ec site/conf/const.php
@@ -0,0 +1,14 @@
+<?php
+
+// MySQL接続情報
+$USER   = 'codecamp46591';           // MySQLのユーザ名
+$PASSWD = 'codecamp46591';           // MySQLのパスワード
+$DBNAME = 'codecamp46591';           // データベース名
+$HOST   = 'localhost';      // データベースのホスト名又はIPアドレス
+$CHARSET = 'utf8';          // データベースの文字コード
+$DSN = 'mysql:dbname='.$DBNAME .';host='.$HOST.';charset='.$CHARSET;  // PDOのDSN
+
+// 画像ファイルのディレクトリ
+$IMG_DIR = './img_item/';
+
+?>
