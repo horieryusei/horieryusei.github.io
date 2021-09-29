4816e3f7c266d3033ee293a24a016b3dc1bceaeb
Horie Ryusei <89172397+horieryusei@users.noreply.github.com>
2021-09-29 23:29:26 +0900

    Create logout.php

---
+++ ec site/logout.php
@@ -0,0 +1,24 @@
+<?php
+
+// 設定ファイル読み込み
+require_once './conf/const.php';
+// 関数ファイル読み込み
+require_once './model/common.php';
+
+// セッション開始
+session_start();
+// セッション名取得
+$session_name = session_name();
+// セッション変数を全て削除
+$_SESSION = array();
+// ユーザのCookieに保存されているセッションIDを削除
+if (isset($_COOKIE[$session_name])) {
+  setcookie($session_name, '', time() - 42000);
+}
+// セッションIDを無効化
+session_destroy();
+
+// loginページへ移動
+redirect_login_page();
+
+?>
